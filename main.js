9e47b9fbfa47e473856de60610d44b8c4f1a722a
phi <phi1618jp@gmail.com>
2012-07-15 09:09:32 +0900

    create tool.

---
+++ main.js
@@ -0,0 +1,52 @@
+/*
+ * phi
+ */
+
+/*
+ * global
+ */
+var lessParser = null;
+var lessEditor = null;
+var cssEditor  = null;
+
+
+/*
+ * main
+ */
+tm.main(function() {
+    // less parser 生成
+    lessParser = new less.Parser();
+    
+    // エディタ
+    lessEditor = ace.edit("editor");
+    lessEditor.setTheme("ace/theme/chrome");
+    lessEditor.getSession().setMode("ace/mode/less");
+    lessEditor.session.$tabSize = 2;
+    //lessEditor.addEventListener("change", parse);
+    lessEditor.commands.addCommand({
+        Name: "parse",
+        bindKey: {
+            win: "Shift-Return",
+        },
+        exec: function(editor) {
+            parse();
+        }
+    });
+    
+    // 結果用エディタ
+    cssEditor = ace.edit("result");
+    cssEditor.setTheme("ace/theme/chrome");
+    cssEditor.getSession().setMode("ace/mode/css");
+});
+
+
+var parse = function() {
+    var value = lessEditor.getValue();
+    lessParser.parse(value, function(err, tree) {
+        if (err) {
+            // cssEditor.setValue(err.toString());
+            return console.error(err);
+        }
+        cssEditor.setValue(tree.toCSS());
+    });
+};
